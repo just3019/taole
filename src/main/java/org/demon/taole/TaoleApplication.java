1e04f01be5323a85244c7f21d7222b038eadf474
songlinhao <455674848@qq.com>
2018-12-04 16:16:17 +0800

    Initial commit

---
+++ src/main/java/org/demon/taole/TaoleApplication.java
@@ -0,0 +1,12 @@
+package org.demon.taole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaoleApplication.class, args);
+    }
+}
---
+++ src/test/java/org/demon/taole/TaoleApplicationTests.java
@@ -0,0 +1,16 @@
+package org.demon.taole;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaoleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
