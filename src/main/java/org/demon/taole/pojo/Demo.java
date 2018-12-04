c0d95285dba499060c590d8749f2005226aef276
demon <455674848@qq.com>
2018-12-04 23:13:23 +0800

    update

---
+++ src/main/java/org/demon/taole/pojo/Demo.java
@@ -0,0 +1,65 @@
+package org.demon.taole.pojo;
+
+import java.util.Date;
+
+public class Demo {
+    private Long id;
+
+    private String name;
+
+    private Date createtime;
+
+    private Date updatetime;
+
+    private Boolean status;
+
+    private Boolean type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public Date getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Date updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public Boolean getType() {
+        return type;
+    }
+
+    public void setType(Boolean type) {
+        this.type = type;
+    }
+}
