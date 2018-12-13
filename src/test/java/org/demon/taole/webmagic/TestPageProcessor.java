2db2c1e5a4b8e8293173ecc206e72838700be6ba
songlinhao <455674848@qq.com>
2018-12-13 19:21:38 +0800

    添加  加载已经渲染好的界面。

---
+++ src/test/java/org/demon/taole/webmagic/TestPageProcessor.java
@@ -0,0 +1,42 @@
+package org.demon.taole.webmagic;
+
+import lombok.extern.apachecommons.CommonsLog;
+import org.demon.pool.SeleniumDownloader;
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * desc:
+ *
+ * @author demon
+ * @date 2018-12-13 09:28
+ */
+@CommonsLog
+public class TestPageProcessor implements PageProcessor {
+
+    // 部分一：抓取网站的相关配置，包括编码、抓取间隔、重试次数等
+    private Site site;
+
+    public static void main(String[] args) {
+        Spider.create(new TestPageProcessor())
+                .addUrl("https://product.suning.com/0000000000/10606649860.html")
+                .setDownloader(new SeleniumDownloader())
+                .thread(5).run();
+    }
+
+    @Override
+    public void process(Page page) {
+        String price = page.getHtml().xpath("//*[@id=\"mainPrice\"]/dl[1]/dd/span[1]/text()").toString();
+        log.info("price:" + price);
+    }
+
+    @Override
+    public Site getSite() {
+        if (site == null) {
+            site = Site.me().setRetryTimes(3).setSleepTime(1000);
+        }
+        return site;
+    }
+}
